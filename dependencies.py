c3412afe95d33ac3f8fc38eb2429bcf71e5b3ba5
Dean <deandi@payoneer.com>
2022-09-19 16:51:55 +0200

    Quick Hello World

---
+++ dependencies.py
@@ -0,0 +1,7 @@
+import sys
+
+sys.path.append("/mnt/web019/b1/88/53274188/htdocs/.local/lib/python3.8/site-packages")
+
+
+# /mnt/web019/b1/88/53274188/htdocs/.local/lib/python3.8/site-packages
+# sftp://ssh.strato.de/.local/lib/python3.8/site-packages
