2fcb65150b1d50017576c52eb013a7f3841616ea
Dean <deandi@payoneer.com>
2022-09-20 10:33:44 +0200

    This wont work

--- hello.py
+++ hello.py
@@ -2,14 +2,12 @@ import dependencies
 from flask import Flask
 
 
-
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello World !</p>"
-    return "<p>Checking Push on commit</p>"
+    return "<p>Hello World ! \n\n Checking Push on commit </p>"
 
 
 # app.run(host="0.0.0.0", port=81)
