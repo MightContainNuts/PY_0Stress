43e6e20fcb89db4171b609b335ca9da66019e0a9
Dean <deandi@payoneer.com>
2022-09-20 10:26:13 +0200

    disabled pylance aas it was a pita

--- hello.py
+++ hello.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello World</p>"
+    return "<p>Hello World !</p>"
+    return "<p>Checking Push on commit</p>"
 
 
 # app.run(host="0.0.0.0", port=81)
