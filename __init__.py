1619634502dcde55aa3ec889d42cb9f4866c5c74
Dean <deandi@payoneer.com>
2022-09-27 09:37:01 +0200

    rel path removed

--- __init__.py
+++ __init__.py
@@ -13,9 +13,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db.init_app(app)
 
 # blueprint for auth routes in our app
-from .auth import auth as auth_blueprint
+from auth import auth as auth_blueprint
 app.register_blueprint(auth_blueprint)
 
 # blueprint for non-auth parts of app
-from .main import main as main_blueprint
+from main import main as main_blueprint
 app.register_blueprint(main_blueprint)
