fb4fc6270320b779c086fca395099571f81e15be
Dean <deandi@payoneer.com>
2022-09-27 14:21:09 +0200

    fd

--- __init__.py
+++ __init__.py
@@ -1,34 +1,12 @@
 import dependencies
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-# init SQLAlchemy so we can use it later in our models
-db = SQLAlchemy()
-
 
 app = Flask( __name__ )
 
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.route('/')
+def base():
+    return 'welcome to the index'
 
-db.init_app(app)
-
-# blueprint for auth routes in our app
-try: 
-    from auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
-    print ('success! - imported auth as blueprint')
-
-except:
-    print ('nope -auth-  that didna work')
-
-
-# blueprint for non-auth parts of app
-
-try: 
-    from main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-    print ('success! - imported main as blueprint')
-
-except:
-    print ('nope - -main- that didna work')
+app.route('/index')
+def base():
+    return 'welcome to the index'
