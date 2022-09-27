ccd56aed31dc0957fe97281f0cff2653e9e9fa4d
Dean <deandi@payoneer.com>
2022-09-27 14:17:15 +0200

    fg

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from flask import Blueprint
 main = Blueprint('main', __name__)
 
 
-@main.route('/')
-def index():
-    return 'Index'
+#@main.route('/')
+#def index():
+#    return 'Index'
 
 @main.route('/profile')
 def profile():
