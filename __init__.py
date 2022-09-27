e130385a3e1fedcb6b342f0e350fd44346db1ed8
Dean <deandi@payoneer.com>
2022-09-27 14:26:34 +0200

    @

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 import dependencies
 from flask import Flask
 
-app = Flask( __name__ )
+app = Flask(__name__)
 
-app.route('/')
+@app.route('/')
 def base():
     return 'welcome to the index'
 
-app.route('/index')
+@app.route('/index')
 def index():
     return 'welcome to the index'
