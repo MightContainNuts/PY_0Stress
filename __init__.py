6351fd50cbfbc6b948191103af0f2e879470e8e0
Dean <deandi@payoneer.com>
2022-09-27 10:27:14 +0200

    back to basics

--- __init__.py
+++ __init__.py
@@ -1,50 +1,24 @@
 import dependencies
-from flask import Flask, Blueprint
-#from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # init SQLAlchemy so we can use it later in our models
-#db = SQLAlchemy()
+db = SQLAlchemy()
+
 
 app = Flask(__name__)
 
-#app.config['SECRET_KEY'] = 'secret-key-goes-here'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-    
-#db.init_app(app)
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
+db.init_app(app)
 
+# blueprint for auth routes in our app
+from auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
 
 # blueprint for non-auth parts of app
-#
+from main import main as main_blueprint
+app.register_blueprint(main_blueprint)
 
-
-main = Blueprint('main', __name__)
-
-@main.route('/')
-def index():
-    return 'Index2'
-
-@main.route('/profile')
-def profile():
-    return 'Profile'
-
-@main.route('/aboutme')
-def aboutme():
-    return 'Aboutme'
-
-# blueprint for auth parts of app
-
-auth = Blueprint('auth', __name__)
-
-@auth.route('/login')
-@auth.route('/login')
-def login():
-    return 'Login'
-
-@auth.route('/signup')
-def signup():
-    return 'Signup'
-
-@auth.route('/logout')
-def logout():
-    return 'Logout'
+return app
