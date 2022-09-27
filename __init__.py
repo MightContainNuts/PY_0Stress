5eeb16daf7a70f50d8c97e2650ff711ee95b6f79
Dean <deandi@payoneer.com>
2022-09-27 18:01:19 +0200

    add posts to index()

--- __init__.py
+++ __init__.py
@@ -22,4 +22,4 @@ def index():
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
