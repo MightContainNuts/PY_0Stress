10fff4c117002bb3ef626295833fd82357d43558
Dean <deandi@payoneer.com>
2022-09-29 15:28:25 +0200

    fixy fixy

--- __init__.py
+++ __init__.py
@@ -24,34 +24,34 @@ def get_post(post_id):
     return post
 
 
-def startApp():
-
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'MightContainNuts@0stress'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-    db.init_app(app)
 
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'MightContainNuts@0stress'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db.init_app(app)
 
-    from models import User
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        # since the user_id is just the primary key of our user table, use it in the query for the user
-        return User.query.get(int(user_id))
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
-    # blueprint for auth routes in our app
-    from auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+from models import User
 
-    # blueprint for non-auth parts of app
-    from main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    return User.query.get(int(user_id))
 
-    return app
+# blueprint for auth routes in our app
+from auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
+
+# blueprint for non-auth parts of app
+from main import main as main_blueprint
+app.register_blueprint(main_blueprint)
+
+
 
 
 
