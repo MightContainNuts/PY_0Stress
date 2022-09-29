c71aceb8a2b2cff2c4f2806384062b3a01200a92
Dean <deandi@payoneer.com>
2022-09-29 12:55:57 +0200

    last last snc

--- __init__.py
+++ __init__.py
@@ -26,8 +26,8 @@ def get_post(post_id):
 #app.register_blueprint(auth_blueprint)
 
 # blueprint for non-auth parts of app
-from .main import main as main_blueprint
-app.register_blueprint(main_blueprint)
+#from .main import main as main_blueprint
+#app.register_blueprint(main_blueprint)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'MightContainNuts@0stress'
